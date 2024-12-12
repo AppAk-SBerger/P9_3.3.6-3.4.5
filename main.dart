e93f5371daf017908ff3de0e7194fd813479c7fd
SBerger_DEV <teebeutel.de@gmail.com>
2024-12-12 15:26:06 +0100

    add almanach, profil and calendar as function

--- main.dart
+++ main.dart
@@ -22,107 +22,6 @@ void main() {
   // while (runProgram = true) {}
 
   splashScreen();
-  mainMenu();
-
-  switch (userInput) {
-    case 'a' || 'A':
-      clearConsole();
-      print("\n");
-      print("\t\t~~~Almanach~~~");
-      print("\n");
-      print("\tWähle bitte ein Objekt aus.");
-      print("\n");
-      print("\t(M)ond\t\tM(a)rs\t\t(V)enus");
-      print("\n");
-      String userInput = stdin.readLineSync() ?? "";
-
-      switch (userInput) {
-        case 'm' || 'M':
-          clearConsole();
-          print("\n");
-          print("\t\t~~~Mond~~~");
-          print("\n");
-          print(
-              "Der Mond (mittelhochdeutsch mâne; lateinisch luna) ist der einzige natürliche Satellit der Erde.");
-          print(
-              "Sein Name ist etymologisch verwandt mit Monat. Weil die Trabanten anderer Planeten des Sonnensystems im übertragenen Sinn meist");
-          print(
-              "ebenfalls als Monde bezeichnet werden, spricht man zur Vermeidung von Verwechslungen mitunter vom Erdmond.");
-          print("\n");
-          print("\n");
-        case 'a' || 'A':
-          clearConsole();
-          print("\n");
-          print("\t\t~~~Mars~~~");
-          print("\n");
-          print("\t\t  ~null~");
-          print("\t\t    :(");
-          print("\n");
-        case 'v' || 'V':
-          clearConsole();
-          print("\n");
-          print("\t\t~~~Venus~~~");
-          print("\n");
-          print("\t\t  ~null~");
-          print("\t\t    :(");
-          print("\n");
-        default:
-          print("Bitte wähle 'M', 'a' oder 'V'.");
-      }
-    case 'p' || 'P':
-      clearConsole();
-      print("\n");
-      print("\t\t~~~~Profil~~~~");
-      print("\n");
-      print("\tName\tID\tAlter\tAktiv?");
-      for (int i = 0; i <= listOfUserNames.length - 1; i++) {
-        print(
-            "\t${listOfUserNames[i]}\t${listOfUserIDs[i]}\t${listOfUserAges[i]}\t${listOfUserIsActive[i]}");
-      }
-      print("\n");
-
-    case 'k' || "K":
-      clearConsole();
-      print("\n");
-      print("\t\t~~~Kalender~~~");
-      print("\n");
-      print("\tWähle bitte eine Option aus.");
-      print("\n");
-      print("\t(Ü)bersicht der Einträge\t(N)euen Eintrag erstellen");
-      print("\n");
-
-      String userInput = stdin.readLineSync() ?? "";
-      switch (userInput) {
-        case 'ü' || 'Ü':
-          for (int i = 0; i < calendarEntries.length; i++) {
-            print("${calendarEntries[i]}");
-          }
-        case 'n' || 'N':
-          print("\n");
-          print("\tWas möchtest du beobachten?");
-          print("\n");
-          String userInput = stdin.readLineSync() ?? "";
-          calendarEntries.add({'description': userInput});
-          print("\n");
-          print("\tWann möchtest du $userInput beobachten? TT.MM.JJJJ");
-          String userInput2 = stdin.readLineSync() ?? "";
-          calendarEntries.add({'date': userInput2});
-          print("\t$userInput beobachten am $userInput2 hinzugefügt.");
-      }
-    //calendarEntries.add({'description': 'Mond gucken', 'date': '27.12.2024'})
-
-// List<Map<String, dynamic>> calendarEntries = [{'description': 'Mond gucken', 'date': '27.12.2024'}];
-    default:
-      print("Bitte wähle 'A', 'P' oder 'K'.");
-  }
-}
-
-void clearConsole() {
-  if (Platform.isWindows) {
-    print(Process.runSync('cls', [], runInShell: true).stdout);
-  } else {
-    print(Process.runSync('clear', [], runInShell: true).stdout);
-  }
 }
 
 void splashScreen() {
@@ -134,11 +33,10 @@ void splashScreen() {
   print("\t\t\t~~~~~~~~~~~~~~~~~~~~~~");
   print("\n");
   sleep(Duration(seconds: 5));
-  // start mainMenu
+  mainMenu();
 }
 
 void mainMenu() {
-  String userDecision = "";
   clearConsole();
   print("\tWähle bitte eine Option aus:");
   print("\n");
@@ -146,11 +44,116 @@ void mainMenu() {
   print("\t(P)rofil");
   print("\t(K)alender");
   print("\n");
-  userDecision = userInputToString();
-  // add switch
+  String userDecision = userInputToString();
+  switch (userDecision) {
+    case 'a' || 'A':
+      almanach();
+    case 'p' || 'P':
+      profil();
+    case 'k' || "K":
+      calendar();
+    default:
+      print("Bitte wähle 'A', 'P' oder 'K'.");
+  }
+}
+
+void almanach() {
+  clearConsole();
+  print("\n");
+  print("\t\t~~~Almanach~~~");
+  print("\n");
+  print("\tWähle bitte ein Objekt aus.");
+  print("\n");
+  print("\t(M)ond\t\tM(a)rs\t\t(V)enus");
+  print("\n");
+  String userDecision = userInputToString();
+
+  switch (userDecision) {
+    case 'm' || 'M':
+      clearConsole();
+      print("\n");
+      print("\t\t~~~Mond~~~");
+      print("\n");
+      print(
+          "Der Mond (mittelhochdeutsch mâne; lateinisch luna) ist der einzige natürliche Satellit der Erde.");
+      print(
+          "Sein Name ist etymologisch verwandt mit Monat. Weil die Trabanten anderer Planeten des Sonnensystems im übertragenen Sinn meist");
+      print(
+          "ebenfalls als Monde bezeichnet werden, spricht man zur Vermeidung von Verwechslungen mitunter vom Erdmond.");
+      print("\n");
+      print("\n");
+    case 'a' || 'A':
+      clearConsole();
+      print("\n");
+      print("\t\t~~~Mars~~~");
+      print("\n");
+      print("\t\t  ~null~");
+      print("\t\t    :(");
+      print("\n");
+    case 'v' || 'V':
+      clearConsole();
+      print("\n");
+      print("\t\t~~~Venus~~~");
+      print("\n");
+      print("\t\t  ~null~");
+      print("\t\t    :(");
+      print("\n");
+    default:
+      print("Bitte wähle 'M', 'a' oder 'V'.");
+  }
+}
+
+void profil() {
+  clearConsole();
+  print("\n");
+  print("\t\t~~~~Profil~~~~");
+  print("\n");
+  print("\tName\tID\tAlter\tAktiv?");
+  for (int i = 0; i <= listOfUserNames.length - 1; i++) {
+    print(
+        "\t${listOfUserNames[i]}\t${listOfUserIDs[i]}\t${listOfUserAges[i]}\t${listOfUserIsActive[i]}");
+  }
+  print("\n");
+}
+
+void calendar() {
+  clearConsole();
+  print("\n");
+  print("\t\t~~~Kalender~~~");
+  print("\n");
+  print("\tWähle bitte eine Option aus.");
+  print("\n");
+  print("\t(Ü)bersicht der Einträge\t(N)euen Eintrag erstellen");
+  print("\n");
+  String userDecision = stdin.readLineSync() ?? "";
+  switch (userDecision) {
+    case 'ü' || 'Ü':
+      for (int i = 0; i < calendarEntries.length; i++) {
+        print("${calendarEntries[i]}");
+      }
+    case 'n' || 'N':
+      print("\n");
+      print("\tWas möchtest du beobachten?");
+      print("\n");
+      String userInput = stdin.readLineSync() ?? "";
+      calendarEntries.add({'description': userInput});
+      print("\n");
+      print("\tWann möchtest du $userInput beobachten? TT.MM.JJJJ");
+      String userInput2 = stdin.readLineSync() ?? "";
+      calendarEntries.add({'date': userInput2});
+      print("\t$userInput beobachten am $userInput2 hinzugefügt.");
+  }
 }
 
 String userInputToString() {
   userInputString = stdin.readLineSync() ?? "";
   return userInputString;
+}
+
+void clearConsole() {
+  if (Platform.isWindows) {
+    print(Process.runSync('cls', [], runInShell: true).stdout);
+  } else {
+    print(Process.runSync('clear', [], runInShell: true).stdout);
+  }
 }
